9d0543f9c43153518dc4ecd4e1dc43da0540f4b7
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-11 15:53:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence1)
   original_sentence = sentence1.split(" ")
   new_sentence = []
-  original_sentence.each{|n| 
+  original_sentence.each{|n| new_sentence << n.reverse_inplace}
   puts new_sentence
 end
