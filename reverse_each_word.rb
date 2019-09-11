7ecf7a69ae3c188fc8e9f768ec6db7e6984dd2a5
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-11 15:55:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence1)
   original_sentence = sentence1.split(" ")
   new_sentence = []
   original_sentence.each{|n| new_sentence << n.reverse}
-  puts new_sentence
+  new_sentence.join(" ")
 end
